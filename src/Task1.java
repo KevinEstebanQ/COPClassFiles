1d2bd09228520c7f8875090075e14e8dba83062a
KevinQuiceno <kevinplaythinks@gmail.com>
2025-09-23 09:46:20 -0400

    task 1 and 2 from the first Algorithms and Data struct homework

---
+++ src/Task1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Task1 {
+    public static ArrayList<Integer> findDominant(int[] Arr) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int Min = 0;
+        for(int i = Arr.length - 1; i >= 0; i--) {
+            if(Arr[i] > Min){
+                temp.add(Arr[i]);
+                Min = Arr[i];
+            }
+        }
+        Collections.reverse(temp);
+        return temp;
+    }
+    public static void main(String[] args) {
+        int[] arr ={16,17,4,3,5,2};
+        System.out.println(findDominant(arr).toString());
+    }
+}
